1fe0da1eea41652b9708094bc1773d443e489db2
Daniel <daniel_b_christensen@hotmail.com>
2016-12-23 18:50:51 +0100

    Added base projects, and some basic string extensions

---
+++ BaseApplication/Extensions/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extensions.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string TrimToLowerInvariant(this String str)
+        {
+            return str.Trim().ToLowerInvariant();
+        }
+
+        public static string TrimToUpperInvariant(this String str)
+        {
+            return str.Trim().ToUpperInvariant();
+        }
+    }
+}
