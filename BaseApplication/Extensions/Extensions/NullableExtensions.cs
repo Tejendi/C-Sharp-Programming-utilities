961295a4c2ae619f6425dc16343f5cd6371db2e5
Daniel Bloch Christensen <daniel@incom-it.dk>
2017-03-16 10:13:02 +0100

    Added tests for Nullable Extensions

--- BaseApplication/Extensions/Extensions/NullableExtensions.cs
+++ BaseApplication/Extensions/Extensions/NullableExtensions.cs
@@ -2,19 +2,7 @@
 {
     public static class NullableExtensions
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="target"></param>
-        /// <returns></returns>
-        public static bool HasValueAndEquals<T>(this T? source, T target) where T : struct
-        {
-            return source.HasValue && source.Value.Equals(target);
-        }
-
-        /// <summary>
+      /// <summary>
         /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
--- BaseApplication/Tests/Tests/NullableExtensionTests.cs
+++ BaseApplication/Tests/Tests/NullableExtensionTests.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Extensions.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Objects;
 
 namespace Tests.Tests
 {
     [TestClass]
     public class NullableExtensionTests
     {
+        [TestMethod]
+        public void HasValueAndEquals()
+        {
+            int? nullValue = null;
+            int? notNullValue = 2;
+
+            Assert.IsFalse(nullValue.HasValueAndEquals(2));
+            Assert.IsTrue(notNullValue.HasValueAndEquals(2));
+        }
+
+
+        [TestMethod]
+        public void HasValueAndEqualsNullTarget()
+        {
+            int? nullValue = null;
+
+            Assert.IsFalse(nullValue.HasValueAndEquals(2));
+            Assert.IsFalse(nullValue.HasValueAndEquals(null));
+        }
     }
 }
